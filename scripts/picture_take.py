5c9ce345749b8a8013d023113014f998e4b6dcbf
luqman <noshluk2>
2023-03-01 09:46:41 +0500

    picture saving test

--- scripts/picture_take.py
+++ scripts/picture_take.py
@@ -1,19 +1,14 @@
-# Python program to explain cv2.imwrite() method
+# This scritps saves the code
 
 import cv2
 
-# Script to test if camera is working or not
-
 def main():
     video_feed = cv2.VideoCapture(0)
-    # while(1):
     _,frame = video_feed.read()
 
-    cv2.imwrite('image', frame)
-    print("\n\nSuccessfully read image from camera\n\n")
-    cv2.imshow("camera_feed.jpg", frame)
-    cv2.waitKey(1)
-
+    cv2.imwrite('picture.jpg', frame)
+    print("\n\nSuccessfully saved picture from camera\n\n")
+    video_feed.release()
 
 if __name__ == '__main__':
     main()
